2218fa61d5a9aca71612895778e0360f75ea5a07
David Fernandes <davidsf@pantherify.com>
2024-11-25 15:57:01 +0000

    configuring for better configs

--- oc_root/dashboard/config.php
+++ oc_root/dashboard/config.php
@@ -3,17 +3,17 @@
 define('APPLICATION', 'Admin');
 
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_SERVER', 'http://localhost:8080/dashboard/');
 define('HTTP_CATALOG', 'http://localhost:8080/');
 
 // DIR
 define('DIR_OPENCART', '/var/www/html/');
-define('DIR_APPLICATION', DIR_OPENCART . 'admin/');
+define('DIR_APPLICATION', DIR_OPENCART . 'dashboard/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
 define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
-define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_STORAGE', '/var/www/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
---
+++ storage/cache/template/3b/3b6b30d77abf625925e508d2aaf7c029.php
@@ -0,0 +1,69 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* dashboard/view/template/common/footer.twig */
+class __TwigTemplate_1f554b4693b08052d6b4fe4110397c2f extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo ($context["text_footer"] ?? null);
+        echo "<br/>";
+        echo ($context["text_version"] ?? null);
+        echo "</footer></div>
+<script src=\"";
+        // line 2
+        echo ($context["bootstrap"] ?? null);
+        echo "\" type=\"text/javascript\"></script>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "dashboard/view/template/common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "dashboard/view/template/common/footer.twig", "/var/www/html/dashboard/view/template/common/footer.twig");
+    }
+}
